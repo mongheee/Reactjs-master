024905f9b9de2590d9317e6af6594dbbd31b36ef
김민희 <monghee0727@gmail.com>
2022-03-05 04:43:56 +0900

    commit

---
+++ src/Circle.tsx
@@ -0,0 +1,22 @@
+/* import styled from "styled-components"; 
+
+interface ContainerProps {
+  bgColor: string;
+}
+const Container = styled.div<ContainerProps>`
+  height: 200px;
+  width: 200px;
+  border-radius: 50%;
+  background-color: ${(props) => props.bgColor};
+`;
+
+interface CircleProps {
+  // TypeScript에게 object shape 설명
+  bgColor: string;
+}
+function Circle({ bgColor }: CircleProps) {
+  return <Container bgColor={bgColor} />;
+}
+
+export default Circle;
+*/
