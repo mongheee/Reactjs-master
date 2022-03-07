cf8ba73ec5030d8a3725f87686e9f3e7c9082e0d
mongheee <monghee0727@gmail.com>
2022-03-07 11:37:31 +0900

    Update

--- src/theme.ts
+++
@@ -1,8 +0,0 @@
-import "styled-components";
-declare module "styled-components" {
-  export interface DefaultTheme {
-    bgColor: "#2f3640";
-    textColor: "f5f6fa";
-    accentColor: "#3F0099";
-  }
-}
